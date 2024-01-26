1059b6e23788077c989b42b5e09bab99bb984235
JuanJoAraujo2003 <juan.araujo.bedon@udla.edu.ec>
2024-01-25 22:32:34 -0500

    Integracion API

---
+++ Models/Empleados.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_P3.Models
+{
+    internal class Empleados
+    {
+    }
+
+    public class Empleado
+    {
+        public int empleadoID { get; set; }
+        public string nombreEmpleado { get; set; }
+        public string cargoEmpleado { get; set; }
+        public int edadEmpleado { get; set; }
+        public double sueldoEmpleado { get; set; }
+        public int sedeID { get; set; }
+        public object sede { get; set; }
+    }
+}
---
+++ Views/EmpleadosHotel.xaml.cs
@@ -0,0 +1,47 @@
+using Newtonsoft.Json;
+using Proyecto_P3.Models;
+
+namespace Proyecto_P3.Views;
+
+public partial class EmpleadosHotel : ContentPage
+{
+	public EmpleadosHotel()
+	{
+		InitializeComponent();
+	}
+
+    private void displayEmpleadosInformation(List<Empleado> empleados)
+    {
+        if (empleados != null && empleados.Any())
+        {
+            foreach (var empleado in empleados)
+            {
+                empleadosLabel.Text += $"\n\nNombre de los empleados: {empleado.nombreEmpleado}\n";
+            }
+        }
+        else
+        {
+            empleadosLabel.Text = "No existen empleados";
+        }
+    }
+
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        if (Connectivity.NetworkAccess == NetworkAccess.Internet)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = $"http://localhost:7204/api/Empleados";
+
+                var response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    var empleados = JsonConvert.DeserializeObject<List<Empleado>>(json);
+
+                    displayEmpleadosInformation(empleados);
+                }
+            }
+        }
+    }
+}
